6718ed1cf20c5e0386308e6f8e1f470a1a1c3187
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-10-04 23:06:01 -0300

    1

--- Javascript/principal.js
+++ Javascript/principal.js
@@ -36,27 +36,41 @@ function montar_placar (pais1, placar1, placar2, pais2, dataLocal, penalti ,tagG
     
 }
 
-let tagBase = document.querySelector("#primeira-fase")
-
-seletorAno.addEventListener("change", function(){
-    tagBase.innerText=""
-    let copa = listar_jogos_por_copa(this.value)
-    for (jogo of copa) {
-        montar_placar(jogo[5],jogo[6],jogo[7],jogo[8],`${jogo[0]} - ${jogo[3]} - ${jogo[4]}`,jogo[9] ,tagBase)
-    }
-})
-
 function montar_grupo(grupo, ano, tagTabela){
     let copa = listar_jogos_por_copa(ano)
     let tituloGrupo = document.createElement("h2")
     let tagGrupo = document.createElement("div")
     tituloGrupo.innerText = grupo
-    tagGrupo.innerText=""
     tagGrupo.appendChild(tituloGrupo)
     for (jogo of copa) {
-        montar_placar(jogo[5],jogo[6],jogo[7],jogo[8],`${jogo[0]} - ${jogo[3]} - ${jogo[4]}`,jogo[9] ,tagGrupo)
+        if (jogo[2].toUpperCase() == grupo){
+            montar_placar(jogo[5],jogo[6],jogo[7],jogo[8],`${jogo[0]} - ${jogo[3]} - ${jogo[4]}`,jogo[9] ,tagGrupo)
+        }
     }
     tagTabela.appendChild(tagGrupo)
-
 }
 
+function montar_copa_do_mundo(ano){
+    tagBase.innerText = ""
+    for (grupo of ordemGrupos){
+        montar_grupo(grupo,ano,tagBase)
+    }
+}
+
+// function listar_grupos_da_copa(ano){
+//     for (grupo of ordemGrupos){
+
+//     }
+
+// }
+
+let tagBase = document.querySelector("#primeira-fase")
+
+
+seletorAno.addEventListener("change", function(){
+    montar_copa_do_mundo(this.value)    
+    
+})
+
+let teste = ["p","p","a","p"]
+console.log(teste.filter())
